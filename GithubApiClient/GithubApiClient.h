6755a808f3559e94f4053d10b262fab2336c61fa
Mohamed Matloub <Mohamed.a.matloub@gmail.com>
2020-07-03 03:36:17 +0200

    Initial Commit

---
+++ GithubApiClient/GithubApiClient.h
@@ -0,0 +1,19 @@
+//
+//  GithubApiClient.h
+//  GithubApiClient
+//
+//  Created by Mohamed Matloub on 7/3/20.
+//  Copyright © 2020 Matloub. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GithubApiClient.
+FOUNDATION_EXPORT double GithubApiClientVersionNumber;
+
+//! Project version string for GithubApiClient.
+FOUNDATION_EXPORT const unsigned char GithubApiClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GithubApiClient/PublicHeader.h>
+
+
